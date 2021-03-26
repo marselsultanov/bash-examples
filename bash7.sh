a8caf44be1b3e0c1ffddf7802f006f8733fe8ee3
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-03-26 21:12:16 +0300

    Delete bash7.sh

--- bash7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for username in test{1..2} super_user; do userdel -r $username; done
-for groupname in test_group my_group; do groupdel $groupname; done
-sed -i '/^super_user/d' /etc/sudoers
