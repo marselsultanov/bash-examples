cfe67044e13f91ea509e5e829385f8b2618f4554
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-03-26 21:02:14 +0300

    Create bash6.sh

---
+++ bash6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for groupname in test_group my_group; do
+groupadd $groupname;
+done
+useradd -m super_user
+echo "super_user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+truncate -s 100k temp
+for username in test{1..2}; do
+useradd -m $username -g test_group -G my_group -c "test users";
+echo "password" | passwd $username --stdin > /dev/null;
+cp temp /home/$username;
+secondary_group=$(id -Gn $username | awk '{print $2}');
+chown $username:$secondary_group /home/$username/temp;
+chmod 345 /home/$username/temp;
+done
