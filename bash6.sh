54d9a1135d2dc5545fb7e8844edb72e907ab0559
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-03-26 21:12:03 +0300

    Update bash6.sh

--- bash6.sh
+++ bash6.sh
@@ -14,3 +14,6 @@ chown $username:$secondary_group /home/$username/temp;
 chmod 345 /home/$username/temp;
 done
 rm temp
+for username in test{1..2} super_user; do userdel -r $username; done
+for groupname in test_group my_group; do groupdel $groupname; done
+sed -i '/^super_user/d' /etc/sudoers
