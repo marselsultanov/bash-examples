386a1304426db70754b810a1b7520dbd8556da86
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-03-26 21:24:03 +0300

    Update bash6.sh

--- bash6.sh
+++ bash6.sh
@@ -5,7 +5,7 @@ done
 useradd -m super_user
 echo "super_user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 truncate -s 100k temp
-for username in test{1..2}; do
+for username in test{1..5}; do
 useradd -m $username -g test_group -G my_group -c "test users";
 echo "password" | passwd $username --stdin > /dev/null;
 cp temp /home/$username;
@@ -14,6 +14,6 @@ chown $username:$secondary_group /home/$username/temp;
 chmod 345 /home/$username/temp;
 done
 rm temp
-for username in test{1..2} super_user; do userdel -r $username; done
+for username in test{1..5} super_user; do userdel -r $username; done
 for groupname in test_group my_group; do groupdel $groupname; done
 sed -i '/^super_user/d' /etc/sudoers
